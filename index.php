6d13f4755c8ff9a7da85e88ba7a01f97149925a9
Red-Phoenix <theom2@orange.fr>
2018-05-31 02:08:44 +0200

    Ajout du routeur

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once('controller/frontend.php');
+
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listProducts') {
+        listProducts();
+    }
+    elseif ($_GET['action'] == 'login') {
+        if (False/*checkSession()*/) {
+            header('Location: index.php?action=listProducts');
+        }
+        elseif (isset($_POST['username']) && isset($_POST['password'])) {
+            login($_POST['username'], $_POST['password']);
+        }
+        else {
+            loginForm();
+        }
+    }
+}
+else {
+    header('Location: index.php?action=listProducts');
+}
