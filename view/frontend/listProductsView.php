44b92f17087db5536b1c6cf3340d6180d0c174c6
Red-Phoenix <theom2@orange.fr>
2018-05-31 15:31:56 +0000

    Create listProductsView

---
+++ view/frontend/listProductsView.php
@@ -0,0 +1,44 @@
+<?php $title = 'Whiskers Shop'; ?>
+
+    <?php require('template.php'); ?>
+
+    <div id="listProductsBlock">
+        <div id="searchBlock">
+
+        </div>
+
+        <div id="listProducts">
+          <div> <img src="imgproduit" />
+            <a href=".....">NomProduit</a>
+             Decription produit
+             Le Prix
+          </div>
+
+          <div> <img src="imgproduit" />
+            <a href=".....">NomProduit</a>
+             Decription produit
+             Le Prix
+          </div>
+
+          <div> <img src="imgproduit" />
+            <a href=".....">NomProduit</a>
+             Decription produit
+             Le Prix
+          </div>
+
+          <div> <img src="imgproduit" />
+            <a href=".....">NomProduit</a>
+             Decription produit
+             Le Prix
+          </div>
+
+        </div>
+
+        <div id="loginBlock">
+
+        </div>
+
+        <div id="cartBlock">
+
+        </div>
+    </div>
