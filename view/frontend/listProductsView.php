66cb445e9da4dc28c0ee3bdcbe019dbb9de289c2
Red-Phoenix <theom2@orange.fr>
2018-06-05 08:43:31 +0200

    Update: listProductsView.php

--- view/frontend/listProductsView.php
+++ view/frontend/listProductsView.php
@@ -1,44 +1,60 @@
-<?php $title = 'Whiskers Shop'; ?>
+<?php
+  $title = 'Whiskers Shop';
+  $banner = 'public/images/banderole.jpg';
+  $link = '<link rel="stylesheet" href="public/css/listProductsView.css">';
+?>
 
-    <?php require('template.php'); ?>
+<?php ob_start(); ?>
+<div id="listProductsBlock">
+    <div id="searchBlock">
 
-    <div id="listProductsBlock">
-        <div id="searchBlock">
-
-        </div>
-
-        <div id="listProducts">
-          <div> <img src="imgproduit" />
-            <a href=".....">NomProduit</a>
-             Decription produit
-             Le Prix
-          </div>
-
-          <div> <img src="imgproduit" />
-            <a href=".....">NomProduit</a>
-             Decription produit
-             Le Prix
-          </div>
-
-          <div> <img src="imgproduit" />
-            <a href=".....">NomProduit</a>
-             Decription produit
-             Le Prix
-          </div>
-
-          <div> <img src="imgproduit" />
-            <a href=".....">NomProduit</a>
-             Decription produit
-             Le Prix
-          </div>
-
-        </div>
-
-        <div id="loginBlock">
-
-        </div>
-
-        <div id="cartBlock">
-
-        </div>
     </div>
+
+    <div id="listProducts">
+      <div> <img src="imgproduit" />
+        <a href=".....">NomProduit</a>
+         Decription produit
+         Le Prix
+      </div>
+
+      <div> <img src="imgproduit" />
+        <a href=".....">NomProduit</a>
+         Decription produit
+         Le Prix
+      </div>
+
+      <div> <img src="imgproduit" />
+        <a href=".....">NomProduit</a>
+         Decription produit
+         Le Prix
+      </div>
+
+      <div> <img src="imgproduit" />
+        <a href=".....">NomProduit</a>
+         Decription produit
+         Le Prix
+      </div>
+
+    </div>
+
+    <div id="loginBlock">
+      <?php
+        if(False /*checkSession()*/) {
+
+            echo '<p>' . $_SESSION['username'] . '</p>';
+        }
+        else{
+          ?>
+            <a href="?action=login">Se connecter</a>
+          <?php
+        }
+      ?>
+    </div>
+
+    <div id="cartBlock">
+
+    </div>
+</div>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
