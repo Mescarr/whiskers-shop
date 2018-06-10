29f38c3dc07ae2b1fb744a80201157fd676d4984
Red-Phoenix <theom2@orange.fr>
2018-06-10 16:17:22 +0000

    Update searchInclude: new input -> Species

--- view/frontend/includes/searchInclude.php
+++ view/frontend/includes/searchInclude.php
@@ -1,6 +1,15 @@
 <div id="searchBlock">
   <form method="GET" action="?action=listProducts">
     <div id="search1">
+      <p id="formSpeciesBlock">
+         Espèce :
+         <select name="species" id="species">
+           <option value="Tous"></option>
+           <option value="Chien">Chien</option>
+           <option value="Chat">Chat</option>
+           <option value="Rongeurs">Rongeurs</option>
+         </select>
+      </p>
       <p id="formCategoryBlock">
          Catégorie :
          <select name="category" id="category">
@@ -16,19 +25,18 @@
         Recherche :
         <input type="search" name="search" />
       </p>
+    </div>
+
+    <div id="search2">
       <p id="formPriceBlock">
        Prix :
         <input type="radio" name="prix" value="croissant" id="Croissant" /> <label for="Croissant">Croissant</label>
         <input type="radio" name="prix" value="decroissant" id="Décroissant" /> <label for="Décroissant">Décroissant</label>
       </p>
-    </div>
-
-    <div id="search2">
       <p id="formRangePriceBlock">
         Min : <input type="number" min="0" max="999999" step="1" value ="0" />€
         Max : <input type="number" min="0" max="999999" step="1" value="999999" />€
       </p>
-
       <input type="submit" value="Rechercher" id="searchButton" />
     </div>
   </form>
